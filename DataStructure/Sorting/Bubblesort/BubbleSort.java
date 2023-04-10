9310d8272e72a4558a27b5204ffd82f500833330
Akshay Mathur <akshaymathur242@gmail.com>
2023-04-10 11:16:48 +0530

    new Approach

--- DataStructure/Sorting/Bubblesort/BubbleSort.java
+++ DataStructure/Sorting/Bubblesort/BubbleSort.java
@@ -7,10 +7,25 @@ public class BubbleSort {
         int [] arr = {2,0,2,1,1,0};
         // int [] arr = {1,2,3,4,5};
         System.out.println(Arrays.toString(arr));
-        bubblesort(arr);
+        buble(arr);
         System.out.println("Sorted array is below");
         System.out.println(Arrays.toString(arr));
         
+    }
+
+    static void  buble(int [] arr ){
+         int n= arr.length;
+
+         for(int i =0;i<n;i++){
+            for(int j =0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+            // System.out.println();
+         }
     }
 
     static void bubblesort( int[] arr){
