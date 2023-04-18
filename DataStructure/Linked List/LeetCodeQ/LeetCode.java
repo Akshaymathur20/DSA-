8531ed90b34c9747d56cc88a72639fcc0feef682
Akshay Mathur <akshaymathur242@gmail.com>
2023-04-19 00:11:18 +0530

    yes

--- DataStructure/Linked List/LeetCodeQ/LeetCode.java
+++ DataStructure/Linked List/LeetCodeQ/LeetCode.java
@@ -2,12 +2,14 @@
 
 
 //Middle of the linked list 
+// Link->  https://leetcode.com/problems/middle-of-the-linked-list/
 /*Here what we did we take two pointer slow and fast 
  * In the first they both point to head;
  * After each slow will point to next value while fast will point to next of next value means Twice of slow till it the fast reaches to end of the linkedlist
  * slow will be at the center of the linked list as per logic 
  * So will return the value of slow which means we will find the the middle of the linked list 
  */
+
 public class LeetCode {
     public ListNode middleNode(ListNode head){
 
