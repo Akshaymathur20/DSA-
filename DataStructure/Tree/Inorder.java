14280a506e38793e258f06ba69aac8f3ccec47d9
Akshay Mathur <akshaymathur242@gmail.com>
2023-06-21 00:44:48 +0530

    yes

--- DataStructure/Tree/Inorder.java
+++ DataStructure/Tree/Inorder.java
@@ -1,3 +1,7 @@
+// Question link --> https://leetcode.com/problems/binary-tree-inorder-traversal/
+
 public class Inorder {
-    
+    public static void main(String[] args) {
+        System.out.println("Inorder Traversel ");
+    }
 }
---
+++ DataStructure/Tree/postorder.java
@@ -0,0 +1,5 @@
+public class postorder {
+    public static void main(String[] args) {
+        
+    }
+}
