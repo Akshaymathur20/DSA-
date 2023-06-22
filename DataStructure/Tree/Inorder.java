7adbaeb4aa05292130e07ddcfd898f2e0742d8ce
Akshay Mathur <akshaymathur242@gmail.com>
2023-06-22 23:53:17 +0530

    yes

--- DataStructure/Tree/Inorder.java
+++ DataStructure/Tree/Inorder.java
@@ -1,7 +1,48 @@
 // Question link --> https://leetcode.com/problems/binary-tree-inorder-traversal/
 
-public class Inorder {
-    public static void main(String[] args) {
-        System.out.println("Inorder Traversel ");
-    }
-}
+class Node {  
+    public int value;  
+    public Node left;  
+    public Node right;  
+    public Node(int data)  
+    {  
+        value = data;  
+        left = right = null;  
+    }  
+}  
+class Inorder {  
+    Node root;  
+    Inorder() { 
+        root = null; 
+    }  
+
+    //for leetCode Solution you can copy this 
+    void traverseInorder(Node node)  
+    {  
+        if (node == null)  
+            return;  
+        traverseInorder(node.left);  
+        System.out.print(node.value + " ");  
+        traverseInorder(node.right);  
+    }  
+
+    void traverseInorder() 
+    {
+         traverseInorder(root);
+    }  
+    public static void main(String args[])  
+    {  
+        Inorder pt = new Inorder();  
+        pt.root = new Node(1);  
+        pt.root.left = new Node(2);  
+        pt.root.right = new Node(3);  
+        pt.root.left.left = new Node(4);  
+        pt.root.left.right = new Node(5);  
+        pt.root.left.left.left = new Node(6);  
+        pt.root.left.left.right = new Node(7);  
+        
+        System.out.println();  
+        pt.traverseInorder();  
+        System.out.println();  
+    }  
+} 
