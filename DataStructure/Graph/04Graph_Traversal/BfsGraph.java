8435104dae48529805abc730da2b69d100c14d59
Akshay Mathur <akshaymathur242@gmail.com>
2024-12-10 02:59:12 +0530

    Traversal

--- DataStructure/Graph/04Graph_Traversal/BfsGraph.java
+++ DataStructure/Graph/04Graph_Traversal/BfsGraph.java
@@ -1,4 +1,3 @@
-// package 04Graph_Traversal;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -8,62 +7,54 @@ public class BfsGraph {
 
     List<List<Integer>> adjList;
     int vertices;
-     
 
-    BfsGraph(int vertices){
-       this.vertices=vertices;
-       adjList = new ArrayList<>();
-       for(int i =0;i<vertices;i++){
-        adjList.add(new ArrayList<>());
-       }
+    BfsGraph(int vertices) {
+        this.vertices = vertices;
+        adjList = new ArrayList<>();
+        for (int i = 0; i < vertices; i++) {
+            adjList.add(new ArrayList<>());
+        }
     }
 
-    public void bfs(int source){
+    public void bfs(int source) {
         Queue<Integer> q = new LinkedList<>();
         boolean[] vis = new boolean[adjList.size()];
 
-        vis[source]= true;
+        vis[source] = true;
         q.add(source);
+
         while (!q.isEmpty()) {
             int curr = q.poll();
-            System.out.print(curr+ " ");
+            System.out.print(curr + " ");
 
-            for(int neighbour:adjList.get(curr)){
-                if(!vis[neighbour]){
-                    vis[neighbour]=true;
+            for (int neighbour : adjList.get(curr)) {
+                if (!vis[neighbour]) {
+                    vis[neighbour] = true;
                     q.add(neighbour);
                 }
             }
         }
-    }    
-    public void addEdgeInList(int source,int destination){
-        adjList.get(source).add(destination);
-         adjList.get(destination).add(source);   //Remove for a directed graph
+    }
 
+    public void addEdgeInList(int source, int destination) {
+        adjList.get(source).add(destination);
+        adjList.get(destination).add(source); // Remove for a directed graph
     }
 
     public static void main(String[] args) {
-        BfsGraph adjList = new BfsGraph(8);
-        
-        adjList.addEdgeInList(1, 3);
+        BfsGraph adjList = new BfsGraph(9);
+
         adjList.addEdgeInList(1, 2);
+        adjList.addEdgeInList(1, 5);
+        adjList.addEdgeInList(2, 6); 
         adjList.addEdgeInList(3, 4);
-        adjList.addEdgeInList(4, 4);
-        adjList.addEdgeInList(4, 6);
-        adjList.addEdgeInList(5, 6);
-        adjList.addEdgeInList(5, 7);
+        adjList.addEdgeInList(3, 6);
+        adjList.addEdgeInList(3, 7);
+        adjList.addEdgeInList(4, 8);
         adjList.addEdgeInList(6, 7);
+        adjList.addEdgeInList(7, 8);
 
-        // adjList.addEdgeInList(1, 3);
-        // adjList.addEdgeInList(1, 2);
-        // adjList.addEdgeInList(3, 4);
-        // adjList.addEdgeInList(4, 5);
-        // adjList.addEdgeInList(4, 6);
-        // adjList.addEdgeInList(5, 6);
-        // adjList.addEdgeInList(5, 7);
-        // adjList.addEdgeInList(6, 8);
-
-        System.out.println("BFS Starting from 0: ");
-        adjList.bfs(4);
-    } 
+        System.out.println("BFS Starting from 2: ");
+        adjList.bfs(2); 
+    }
 }
--- DataStructure/Graph/04Graph_Traversal/DfsGraph.java
+++ DataStructure/Graph/04Graph_Traversal/DfsGraph.java
@@ -1,7 +1,58 @@
-// package 04Graph_Traversal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
 public class DfsGraph {
+
+    List<List<Integer>> adjList;
+    int vertices;
+
+    DfsGraph(int vertices) {
+        this.vertices = vertices;
+        adjList = new ArrayList<>();
+        for (int i = 0; i < vertices; i++) {
+            adjList.add(new ArrayList<>());
+        }
+    }
+
+    public void dfs(int source) {
+        Stack<Integer> st = new Stack<>();
+        boolean vis[] = new boolean[adjList.size()];
+
+        vis[source] = true;
+        st.add(source);
+
+        while (!st.isEmpty()) {
+            int top = st.pop();
+            System.out.print(top + " ");
+            for (int neighbour : adjList.get(top)) {
+                if (!vis[neighbour]) {
+                    vis[neighbour] = true;
+                    st.add(neighbour);
+                }
+            }
+        }
+    }
+
+    public void addEdgeInList(int source, int destination) {
+        adjList.get(source).add(destination);
+        adjList.get(destination).add(source); // Remove for a directed graph
+    }
+
     public static void main(String[] args) {
-        
+        DfsGraph adjList = new DfsGraph(9);
+
+        adjList.addEdgeInList(1, 2);
+        adjList.addEdgeInList(1, 5);
+        adjList.addEdgeInList(2, 6); 
+        adjList.addEdgeInList(3, 4);
+        adjList.addEdgeInList(3, 6);
+        adjList.addEdgeInList(3, 7);
+        adjList.addEdgeInList(4, 8);
+        adjList.addEdgeInList(6, 7);
+        adjList.addEdgeInList(7, 8);
+
+        System.out.println("DFS Starting from 0: ");
+        adjList.dfs(2);   
     }
 }
